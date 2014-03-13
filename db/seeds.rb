bf5ad877309e258943ed9c444e6ed0bfa6b2fda2
Børge Lotre <borge.lotre@gmail.com>
2014-03-13 14:33:16 +0100

    db seed with all teams

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+leagues = League.create([{name: 'Chelsea', position: 0}, 
+  {name: 'Liverpool', position: 0}, 
+  {name: 'Arsenal', position: 0}, 
+  {name: 'Manchester City', position: 0}, 
+  {name: 'Tottenham Hotspur', position: 0}, 
+  {name: 'Manchester United', position: 0}, 
+  {name: 'Everton', position: 0}, 
+  {name: 'Newcastle United', position: 0}, 
+  {name: 'Southampton', position: 0}, 
+  {name: 'West Ham United', position: 0}, 
+  {name: 'Aston Villa', position: 0}, 
+  {name: 'Stoke City', position: 0}, 
+  {name: 'Hull City', position: 0}, 
+  {name: 'Swansea City', position: 0}, 
+  {name: 'Norwich City', position: 0}, 
+  {name: 'Crystal Palace', position: 0}, 
+  {name: 'West Bromwich Albion', position: 0}, 
+  {name: 'Cardiff City', position: 0},
+  {name: 'Sunderland', position: 0},
+  {name: 'Fulham', position: 0}
+  ])
