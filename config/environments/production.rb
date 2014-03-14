2497e8c981c3d9c632beb2caa9948e7e8574adce
Børge Lotre <borge.lotre@gmail.com>
2014-03-14 17:40:28 +0100

    static link to custom.css hack

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,6 @@ TopBetting::Application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both thread web servers
   # and those relying on copy on write to perform better.
@@ -21,14 +20,14 @@ TopBetting::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
