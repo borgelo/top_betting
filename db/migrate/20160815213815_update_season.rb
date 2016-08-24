0569f37ffbc77c4a642bfe24dd7c0500dcb344af
Børge Lotre <borge.lotre@seafood.no>
2016-08-24 22:14:03 +0200

    updating sql - postgress problems

--- db/migrate/20160815213815_update_season.rb
+++ db/migrate/20160815213815_update_season.rb
@@ -1,6 +1,6 @@
 class UpdateSeason < ActiveRecord::Migration
   def change
-    execute "update bets set seasonStartYear = '2014'"
-    execute "update leagues set seasonStartYear = '2014'"
+    execute "update bet set seasonStartYear = '2014'"
+    execute "update league set seasonStartYear = '2014'"
   end
 end
