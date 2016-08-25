37fc1e5ca679bab293943535395c67ca70bd315d
Børge Lotre <borge.lotre@seafood.no>
2016-08-25 19:18:45 +0200

    postgress column case senitive error

--- db/migrate/20160815213815_update_season.rb
+++ db/migrate/20160815213815_update_season.rb
@@ -2,6 +2,5 @@ class UpdateSeason < ActiveRecord::Migration
   def change
     execute 'update Bets set "seasonStartYear" = 2014'
     execute 'update Leagues set "seasonStartYear" = 2014'
-    seasonStartYear
   end
 end
